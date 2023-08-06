448cf07fbf92a84f9e617b37812a221e0dc9be9c
Nas Samoyloff <samo0018@algonquinlive.com>
2023-08-06 19:37:45 -0400

    Layout working, but still error..

--- docs/.vuepress/.temp/pages/guide/index.html.vue
+++ docs/.vuepress/.temp/pages/guide/index.html.vue
@@ -4,11 +4,6 @@
 <li>The trouble with programmers is that you can never tell what a programmer is doing until it’s too late. (Seymour Cray)</li>
 <li>That’s what’s cool about working with computers. They don’t argue, they remember everything, and they don’t drink all your beer. (Paul Leary)</li>
 </ul>
-<h2 id="what-i-do-1" tabindex="-1"><a class="header-anchor" href="#what-i-do-1" aria-hidden="true">#</a> What I do:</h2>
-<ul>
-<li>The trouble with programmers is that you can never tell what a programmer is doing until it’s too late. (Seymour Cray)</li>
-<li>That’s what’s cool about working with computers. They don’t argue, they remember everything, and they don’t drink all your beer. (Paul Leary)</li>
-</ul>
 </div></template>
 
 
